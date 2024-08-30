7df40407cf3d28816b380b7ebba92b3a0ad4b41b
Maria Clara Oliveira Fortes <mfortes@cgee.org.br>
2024-08-29 22:21:26 -0300

    Intalando dependencias do banco de dados sqlite3

--- src/models/customers.ts
+++ src/models/customers.ts
@@ -4,15 +4,3 @@ export type Customer = {
     id: number;
     customer_code: string;
 }
-
-
-const getProduct = async (id: number) => {
-    const retorno = await dbQueryFirst(`SELECT * FROM customer WHERE id = ?`, [id]);
-    return retorno as Customer | undefined;
-}
-
-const insertCustomer = async (customer: Customer) =>{
-    await dbQuery(`INSERT INTO customer (customer_code) VALUES (?)`, [customer.customer_code])
-    let retorno = await dbQuery("SELECT seq AS id FROM sqlite_sequence WHERE name = 'customer'");   
-    return getProduct(retorno[0].id)
-}
